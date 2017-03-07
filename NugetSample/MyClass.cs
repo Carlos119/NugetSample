70f8c7913f038e57ac09d7d4ab3f2a2fb75189ba
CarlitosJr1 <CarlitosJr1@CARLOSALBER4EE7>
2017-03-07 09:39:44 -0600

    Add project files.

---
+++ NugetSample/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetSample
+{
+    class MyClass
+    {
+        async Task DownloadAsync() {
+
+            HttpClient httoClient = new HttpClient();
+            await httoClient.GetStringAsync("");
+        }
+    }
+}
